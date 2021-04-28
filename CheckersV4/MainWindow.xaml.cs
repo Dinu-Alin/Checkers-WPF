6fc5eb390b2f25ee80c66657a690b7605b654f68
Dinu Ionut-Alin <dinui.alin@gmail.com>
2021-04-28 04:02:33 +0300

    Add project files.

---
+++ CheckersV4/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using CheckersV4.Services;
+using CheckersV4.Models;
+using CheckersV4.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using CheckersV4.Views;
+
+namespace CheckersV4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            BoardVM.CurentWindows = this;
+            InitializeComponent();
+        }
+
+
+        private void newGame_Click(object sender, RoutedEventArgs e)
+        {
+            SplashNewGame newGame = new SplashNewGame();
+            newGame.Show();
+            this.Close();
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "xml files (*.xml)|*.xml|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    myStream.Close();
+
+                    OldGame oldGame = new OldGame();
+                    oldGame.Pieces = (DataContext as BoardVM).Pieces;
+                    oldGame.Player1Serialize = BoardVM.Player1;
+                    oldGame.Player2Serialize = BoardVM.Player2;
+
+                    SerializeAndDeserialize.SerializeObjectToXML<OldGame>(oldGame, saveFileDialog1.FileName);
+                }
+            }
+        }
+
+
+        private void open_Click(object sender, RoutedEventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "D:\\";
+                openFileDialog.Filter = "(*.xml)|*.xml";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                    if (filePath.Length > 0)
+                    {
+                        OldGame oldGame = new OldGame();
+                        oldGame = SerializeAndDeserialize.DeserializeObjectToXML<OldGame>(filePath);
+                        var temp = new BoardVM(oldGame);
+
+                        DataContext = temp;
+                    }
+                }
+            }
+        }
+        private void history_Click(object sender, RoutedEventArgs e)
+        {
+            ScoreBoard scoreBoard = new ScoreBoard();
+            scoreBoard.Show();
+            
+        }
+        private void help_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = System.Windows.MessageBox.Show("Checkers, also called draughts, board game, one of the world’s oldest games. Checkers is played by two persons who oppose each other across a board of 64 light and dark squares, the same as a chessboard. The 24 playing pieces are disk-shaped and of contrasting colours (whatever their colours, they are identified as black and white). At the start of the game, each contestant has 12 pieces arranged on the board. While the actual playing is always done on the dark squares, the board is often shown in reverse for clarity. The notation used in describing the game is based on numbering the squares on the board. The black pieces always occupy squares 1 to 12, and the white pieces invariably rest on squares 21 to 32.\nGroup number: 10LF292\nName:Dinu Ionut-Alin");
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            SerializeAndDeserialize.SerializeObjectToXML<HashSet<Player>>((DataContext as BoardVM).Players, @"../../Resources/players.xml");
+            Close();
+        }
+
+        public void StartNewGame()
+        {
+            newGame_Click(null, null);
+        }
+    }
+}
---
+++ CheckersV4/Services/SerializeAndDeserialize.cs
@@ -0,0 +1,54 @@
+﻿using CheckersV4.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CheckersV4.Services
+{
+    class SerializeAndDeserialize
+    {
+        public static void SerializeObjectToXML<T>(T item, string FilePath)
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(T));
+            using (StreamWriter wr = new StreamWriter(FilePath))
+            {
+                xs.Serialize(wr, item);
+            }
+        }
+
+        //public static void SerializeGame<T, W>(T board, W players, string FilePath)
+        //{
+        //    XmlSerializer xsBoard = new XmlSerializer(typeof(T));
+        //    XmlSerializer xsPlayers = new XmlSerializer(typeof(W));
+
+        //    using (StreamWriter wr = new StreamWriter(FilePath))
+        //    {
+        //        xsBoard.Serialize(wr, board);
+        //        xsPlayers.Serialize(wr, players);
+        //    }
+        //}
+
+        public static T DeserializeObjectToXML<T>(string FilePath)
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(T));
+            using (StreamReader rd = new StreamReader(FilePath))
+            {
+                return (T)xs.Deserialize(rd);
+            }
+        }
+
+        //public static KeyValuePair<T, W> DeserializeGame<T, W>(string FilePath)
+        //{
+        //    XmlSerializer xsBoard = new XmlSerializer(typeof(T));
+        //    XmlSerializer xsPlayers = new XmlSerializer(typeof(W));
+        //    using (StreamReader rd = new StreamReader(FilePath))
+        //    {
+        //        return new KeyValuePair<T, W>((T)xsBoard.Deserialize(rd), (W)xsPlayers.Deserialize(rd));
+        //    }
+        //}
+    }
+}
